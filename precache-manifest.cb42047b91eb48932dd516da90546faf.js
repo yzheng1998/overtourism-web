452631c481c7aba00f33ac27a9ce8162047f80e4
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2020-05-22 10:39:13 -0400

    Updates

---
+++ precache-manifest.cb42047b91eb48932dd516da90546faf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ab5d5014cccfca48043c0800d1cc52",
+    "url": "/overtourism-web/index.html"
+  },
+  {
+    "revision": "8a47a110459aa53abe4f",
+    "url": "/overtourism-web/static/js/2.e833eb88.chunk.js"
+  },
+  {
+    "revision": "14dc907096ff79e6b7be55307d0260dd",
+    "url": "/overtourism-web/static/js/2.e833eb88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dd14ead0846a91073a8",
+    "url": "/overtourism-web/static/js/main.1236359e.chunk.js"
+  },
+  {
+    "revision": "8aac214a2d6593bb517f",
+    "url": "/overtourism-web/static/js/runtime-main.a9041afb.js"
+  }
+]);
